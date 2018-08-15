b2e6feac4f876daedd7350e5daa5ffc11ea07623
WebNadin <nenadinweb@gmail.com>
2018-08-15 22:53:09 +0300

    main is ready without modal

--- source/src/js/main.js
+++ source/src/js/main.js
@@ -1,3 +1,14 @@
+$(document).ready(function () {
+    /*$('.wr_nav').css('background-color', 'blue');*/
+    $('.navbar-toggler').click(function () {
+        $('.wr_nav').css('background-color', 'blue');
+        if ($('#navbarToggleExternalContent').hasClass('show')) {
+            console.log('ok');
+            $('.wr_nav').css('background-color', 'transparent');
+        } else {
+            $('.wr_nav').css('background-color', '#012d5a');
 
-/*alert('ola');*/
+        }
+    })
+})
 
