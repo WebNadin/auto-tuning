f04dcc08df1aaa57f53ae85d4e2af92ae8252032
WebNadin <nenadinweb@gmail.com>
2018-10-01 20:24:57 +0300

    gallery is ok. answers is ok.

--- source/src/js/main.js
+++ source/src/js/main.js
@@ -11,20 +11,22 @@ $(document).ready(function () {
         }
     });
     /*$('.my-slider.single-item').slick();*/
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav'
-    });
-    $('.slider-nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: true,
-        centerMode: true,
-        focusOnSelect: true
-    });
+    var $width = $(document).width();
+    if ($width < 767) {
+        $('.autoplay').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+        });
+    }
+    else {
+        $('.autoplay').slick({
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            autoplay: true,
+            autoplaySpeed: 6000,
+        });
+    }
 });
 
