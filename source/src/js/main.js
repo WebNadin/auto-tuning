1ab77829577cafe9b4f1a4596d444f871d2cddc7
WebNadin <nenadinweb@gmail.com>
2018-10-02 20:20:17 +0300

    dev map-marker animation are finished

--- source/src/js/main.js
+++ source/src/js/main.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-    /*$('.wr_nav').css('background-color', 'blue');*/
     $('.navbar-toggler').click(function () {
         $('.wr_nav').css('background-color', 'blue');
         if ($('#navbarToggleExternalContent').hasClass('show')) {
             console.log('ok');
             $('.wr_nav').css('background-color', 'transparent');
         } else {
-            $('.wr_nav').css('background-color', '#012d5a');
+            $('.wr_nav').css('background-color', '#e58b0e');
 
         }
     });
@@ -28,5 +27,8 @@ $(document).ready(function () {
             autoplaySpeed: 6000,
         });
     }
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
 });
 
